2d138ae065ad47368812287a7910271ab01fc22e
Prasenjit Singh <prasenjit0625@gmail.com>
2013-04-26 01:17:09 +0530

    Changes to views.py

--- ajax-search/views.py
+++ ajax-search/views.py
@@ -19,55 +19,66 @@ import simplejson
 import urllib
 import datetime
 import os, sys, Image
-
+from ajax-search.forms import SearchForm
 from django.conf import settings
 
-c=settings.AJAX_SEARCH_FUNCTION
-m= __import__(c)
-
 def xhr_search(request):
+	num=settings.AJAX_SEARCH_LIMIT
+	c=settings.AJAX_SEARCH_HELPER
+	parts=c.split('.')
+	m=__import__(parts[0])
+	for comp in parts[1:]:
+		m=getattr(m,comp)
+	
 	req = {}
 	if request.is_ajax():
 		query = request.POST['query']
-		try:
-			choice = request.POST['choice']
-		except:
-			choice = '1'
-		if choice == '1':
-			words = query.split()
-			count = len(words)
-			model_list = Entry.objects.filter(title__icontains=query, status=1)
-			for L in range(1, count+1):
-				for subset in itertools.permutations(words, L):
-					count1=1
-					query1=subset[0]
-					while count1!=len(subset):
-						query1=query1+" "+subset[count1]
-						count1+=1
-					model_list = model_list | Entry.objects.filter(title__icontains=query1, status=1)
-			model_list = model_list.distinct()
-			req ['name'] =''
-			for e in model_list[0:8]:
-				req ['name']+="""<div class="searchdropdowndiv"><a href='"""
-				req ['name']+=e.get_absolute_url()
-				req ['name']+="""' style="text-decoration:none;"><p style="font-size:14px; color:#000000;">""" 
-				req ['name']+=e
-				req ['name']+="""</p></a></div>"""
-			if model_list.count()>8:
-				req ['name']+="""<div class="searchdropdowndiv" style="border-top:1px solid #e8e8e8;"><a href='/search/?q="""
-				req ['name']+=query
-				req ['name']+="""&choice="""
-				req ['name']+=str(choice)
-				req ['name']+="""' style="text-decoration:none;"><p style="font-size:14px; color:#a2a2a2;">See all results</p></a></div>""" 
+		
+		words = query.split()
+		count = len(words)
+		
+		model_list = m()
+		
+		req ['name'] =''
+		for e in model_list[0:num]:
+			req ['name']+="""<div class="searchdropdowndiv"><a href='"""
+			req ['name']+=e.get_absolute_url()
+			req ['name']+="""' style="text-decoration:none;"><p style="font-size:14px; color:#000000;">""" 
+			req ['name']+=e
+			req ['name']+="""</p></a></div>"""
+		if model_list.count()>num:
+			req ['name']+="""<div class="searchdropdowndiv" style="border-top:1px solid #e8e8e8;"><a href='ajax-search/search/?q="""
+			req ['name']+=query
+			req ['name']+="""' style="text-decoration:none;"><p style="font-size:14px; color:#a2a2a2;">See all results</p></a></div>""" 
 			
 			
-		if choice == '2':
+		
 #					user_list = user_list | User.objects.filter(Q(penname__icontains=query1) | Q(first_name__icontains=query1) | Q(last_name__icontains=query1))
 #			user_list.distinct().order_by('-numfollowers')	
-			
-			#Your code here
 			
 	else:
 		req ['name'] = ""
 	response=simplejson.dumps(req)
 	return HttpResponse(response, mimetype="application/json")	
+
+def search(request):
+	c=settings.AJAX_SEARCH_HELPER
+	parts=c.split('.')
+	m=__import__(parts[0])
+	for comp in parts[1:]:
+		m=getattr(m,comp)
+
+	template_name = "search.html"
+	c=RequestContext(request)
+	
+	if 'q' in request.GET:
+		query=request.GET['q']
+		if query == "":
+			return render_to_response(template_name, {'searchform':SearchForm(), 'query':query, 'timenow':datetime.datetime.now()}, c)
+		
+		words = query.split()
+		count = len(words)
+		model_list = m()
+		return render_to_response(template_name, {'searchform':SearchForm(), 'query':query, 'latestt':model_list, 'timenow':datetime.datetime.now()},c)
+	else:
+		return render_to_response(template_name, {'searchform':SearchForm()})
