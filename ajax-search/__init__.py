011c827d4b7c473201b4fe7c88a75602fdbc7cd6
Prasenjit Singh <prasenjit0625@gmail.com>
2013-04-26 00:15:37 +0530

    First draft

---
+++ ajax-search/__init__.py
@@ -0,0 +1,4 @@
+"A customizable AJAX-powered search for Django."
+
+
+__version__ = '0.6.1'
---
+++ ajax-search/media/js/ajax-search.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+if((!($("#searchdropdown:hover").length)) && (!($("#ajaxsearch:hover").length))){
+ $("#searchdropdown").html('');
+  $("#searchdropdown").hide();
+ }
+/*$("#searchdropdown").blur(function() {
+  $("#searchdropdown").html('');
+  $("#searchdropdown").hide();
+
+});*/
+
+
+$("#searchdropdown").hover(function() {
+      $("#searchdropdown").stop().show();
+  }, function(){
+      $("#searchdropdown").html('');
+  $("#searchdropdown").stop().hide();
+  });
+});
+	
+$(document).ready(function(){
+$("#ajaxsearch").focus(function() {
+$(this).data("hasfocus", true);
+  
+  if ($("#ajaxsearch").val()){
+  $("#searchdropdown").show();
+ if (request){
+request.abort();}
+  request=  $.post("http://firstquarto.com/resources/xhr_search", { 
+    query: $("#ajaxsearch").val() 
+},
+    function(data) {
+        $("#searchdropdown").html(data.name);
+    }
+);}
+});
+
+$("#ajaxsearch").mouseover(function() {
+  if ($("#ajaxsearch").val()){
+  $("#searchdropdown").show();
+ if (request){
+request.abort();}
+  request=  $.post("http://firstquarto.com/resources/xhr_search", { 
+    query: $("#ajaxsearch").val()
+},
+    function(data) {
+        $("#searchdropdown").html(data.name);
+    }
+);}
+});
+
+$("#ajaxsearch").blur(function() {
+$(this).data("hasfocus", false);
+if (!($("#searchdropdown:hover").length)){
+  $("#searchdropdown").html('');
+  $("#searchdropdown").hide();
+ }
+});
+});
+
+
+$(document).ready(function(){
+// run every 3s
+$("#ajaxsearch").keyup(function() {
+
+ if (request){
+request.abort();}
+if ($("#ajaxsearch").val()){
+  request= $.post("http://firstquarto.com/resources/xhr_search", { 
+    query: $("#ajaxsearch").val() 
+},
+    function(data) {
+		$("#searchdropdown").show();
+        $("#searchdropdown").html(data.name);
+    }
+);}
+});
+});
+
+
+/*
+$(document).ready(function() {
+    var $submit = $("#ajaxsearch"),
+        $inputs = $('#searchbuttonmain');
+
+    function checkEmpty() {
+
+        // filter over the empty inputs
+
+        return $inputs.filter(function() {
+            return !$.trim(this.value);
+        }).length === 0;
+    }
+
+    $inputs.on('blur', function() {
+        $submit.prop("disabled", !checkEmpty());
+    }).blur(); // trigger an initial blur
+});*/
+
+
+
+
+
+
+
+function ajaxsearch(){
+$.post("http://firstquarto.com/resources/xhr_search", { 
+    query: $("#ajaxsearch").val()
+},
+    function(data) {
+        $("#searchdropdown").html(data.name);
+    }
+);
+}
