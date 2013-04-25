81d4f74ae5929816a5cf1c6dfc3793fd92981a3a
Prasenjit Singh <prasenjit0625@gmail.com>
2013-04-26 00:25:28 +0530

    Revert "First draft"
    This reverts commit 011c827d4b7c473201b4fe7c88a75602fdbc7cd6.

--- ajax-search/__init__.py
+++
@@ -1,4 +0,0 @@
-"A customizable AJAX-powered search for Django."
-
-
-__version__ = '0.6.1'
--- ajax-search/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-from ajax-search import views
-
-urlpatterns = patterns('',
-	url(r'^xhr_search$','views.xhr_search'),
-)
