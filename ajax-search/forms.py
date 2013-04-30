3fc2c8e535a35bf94d2bb50fd21d1f7c334a82cb
Prasenjit Singh <prasenjit0625@gmail.com>
2013-04-30 12:37:35 +0530

    Update forms.py

--- ajax-search/forms.py
+++ ajax-search/forms.py
@@ -6,12 +6,6 @@ from django.utils.safestring import mark_safe
 
 
 class SearchForm(forms.Form):
-	SEARCH_ENTRY = 1
-	SEARCH_USER = 2
-	SEARCH_CHOICES = (
-		(SEARCH_ENTRY, 'Articles'),
-		(SEARCH_USER, 'Users'),
-	)
 	
 	q = forms.CharField(widget=forms.TextInput(attrs={'id':'ajaxsearch', 'class':'ajaxsearchmain', 'placeholder':'Search articles and users'}), required=False)
-	choice=forms.ChoiceField(widget=forms.Select(attrs={'id':'ajaxsearchchoicefield', 'class':'ajaxsearchchoice'}), choices=SEARCH_CHOICES, initial=SEARCH_INITIAL)
+	
